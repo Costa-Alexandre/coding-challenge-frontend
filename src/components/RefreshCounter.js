519e97d3c1ea4d5b8107e59c71911863dc88a71a
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-22 19:47:58 +0200

    feat: countdown triggers callback when reaches 0

--- src/components/RefreshCounter.js
+++ src/components/RefreshCounter.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../RefreshCounter.css';
 
 export default function RefreshCounter() {
-  let countdown = 60;
+  const [counter, setCounter] = useState(60);
+
+  useEffect(() => {
+    const timer =
+      counter > 0 &&
+      setInterval(() => {
+        if (counter > 1) {
+          setCounter(counter - 1);
+        } else if (counter === 1) {
+          console.log('refresh');
+          setCounter(60);
+        }
+      }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [counter]);
 
   return (
     <div className="refresh-container">
       <span id="refresh-main">Refresh in </span>
-      <span id="time">{countdown}</span>
+      <span id="time">{counter}</span>
     </div>
   );
 }
