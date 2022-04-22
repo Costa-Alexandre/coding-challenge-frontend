9678f2eb6762a82c59bcf90a6f40f7ce6ddf35ba
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-23 00:07:07 +0200

    style: leftTable style

--- src/components/LeftTable.js
+++ src/components/LeftTable.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import '../styles/LeftTable.css';
 
 export default function LeftTable() {
-  return <div>LEFT TABLE</div>;
+  return (
+    <div id="left-table" className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>NR</th>
+            <th>DATE</th>
+            <th>PRODUCT NAME</th>
+            <th className="right-align">ORDER VOLUME</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>353</td>
+            <td>31.01.2022</td>
+            <td>Black Sand Magic</td>
+            <td className="bold right-align">135,33 €</td>
+          </tr>
+          <tr>
+            <td>352</td>
+            <td>30.01.2022</td>
+            <td>White Flower Perfume</td>
+            <td className="bold right-align">5.101,94 €</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
