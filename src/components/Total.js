9244a74aa3ded857836154128405f103b1743b52
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-22 21:15:41 +0200

    refactor: use helper function to format currency

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import '../Total.css';
+import currencyFormat from '../helpers/currencyFormat';
 
 export default function Total({ sum }) {
-  const formatter = new Intl.NumberFormat('de-DE', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-
   return (
     <div id="sum" aria-label="sum">
-      {formatter.format(sum)} €
+      {currencyFormat(sum)}
     </div>
   );
 }
