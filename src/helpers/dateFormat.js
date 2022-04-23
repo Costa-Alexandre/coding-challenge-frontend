c3fb13c996a6ad2bc181ec86ad43924bc60e8ad7
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-23 20:04:59 +0200

    chore: format date to DD.MM.YYYY

--- src/helpers/dateFormat.js
+++ src/helpers/dateFormat.js
@@ -15,4 +15,14 @@ function getMonthFromString(mon) {
   return -1;
 }
 
-export { toDate, toMonthYYYY, getMonthFromString };
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+const dateToDDMMYYYY = (date) => {
+  return `${padTo2Digits(date.getDate())}.${padTo2Digits(
+    date.getMonth() + 1,
+  )}.${date.getFullYear()}`;
+};
+
+export { toDate, toMonthYYYY, getMonthFromString, dateToDDMMYYYY };
