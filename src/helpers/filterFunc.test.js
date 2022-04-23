1c16e96ec694222a8727ead673f3421efc8e6228
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-23 15:26:23 +0200

    test: filterOrdersMonth

--- src/helpers/filterFunc.test.js
+++ src/helpers/filterFunc.test.js
@@ -1,8 +1,45 @@
-import { sumOrders } from './filterFunc';
+import { sumOrders, filterOrdersMonth } from './filterFunc';
 
-test('sumOrders', () => {
-  const x = [{ orderVolume: 1 }, { orderVolume: 1000 }, { orderVolume: -5 }];
-  const result = sumOrders(x);
+describe('sumOrders()', () => {
+  it('should return the sum of orders', () => {
+    const x = [{ orderVolume: 1 }, { orderVolume: 1000 }, { orderVolume: -5 }];
+    const result = sumOrders(x);
 
-  expect(result).toBe(996);
+    expect(result).toBe(996);
+  });
+
+  it('should return 0 if array is empty', () => {
+    const x = [];
+    const result = sumOrders(x);
+
+    expect(result).toBe(0);
+  });
+});
+
+describe('filterOrdersMonth()', () => {
+  it('should return orders for the given month', () => {
+    const x = [
+      { orderDate: new Date(2020, 0, 1) },
+      { orderDate: new Date(2021, 1, 1) },
+      { orderDate: new Date(2022, 1, 1) },
+      { orderDate: new Date(2020, 1, 1) },
+      { orderDate: new Date(2020, 4, 1) },
+    ];
+    const result = filterOrdersMonth(x, 2);
+
+    expect(result.length).toBe(3);
+  });
+
+  it('should return empty array if no orders for the given month', () => {
+    const x = [
+      { orderDate: new Date(2020, 0, 1) },
+      { orderDate: new Date(2020, 1, 1) },
+      { orderDate: new Date(2020, 2, 1) },
+      { orderDate: new Date(2020, 3, 1) },
+      { orderDate: new Date(2020, 4, 1) },
+    ];
+    const result = filterOrdersMonth(x, 12);
+
+    expect(result.length).toBe(0);
+  });
 });
