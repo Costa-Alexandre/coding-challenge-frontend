f14f9dec3a780ec3ed7daba86d5ddad22f04ad25
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-23 17:50:02 +0200

    feat: disabled buttons props are being passed to Arrows

--- src/components/Arrows.js
+++ src/components/Arrows.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/Arrows.css';
 
-export default function Arrows({ callback }) {
+export default function Arrows({ callback, disabledPrev, disabledNext }) {
   return (
     <div id="arrows-container">
       <button
         aria-label="previous month"
         className="arrow-buttons"
         onClick={() => callback('prev')}
+        disabled={disabledPrev}
       >
         <i className="square">
           <i className="left arrow"></i>
@@ -17,6 +18,7 @@ export default function Arrows({ callback }) {
         aria-label="next month"
         className="arrow-buttons"
         onClick={() => callback('next')}
+        disabled={disabledNext}
       >
         <i className="square">
           <i className="right arrow"></i>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Arrows from './Arrows';
 import { toMonthYYYY } from '../helpers/dateFormat';
 import '../styles/NavBar.css';
 
-export default function NavBar({ currentMonth, callback }) {
+export default function NavBar({ currentMonth, callback, intervalArray }) {
+  const [disabledPrev, setDisablePrev] = useState(false);
+  const [disabledNext, setDisableNext] = useState(false);
+
   const handleClick = (direction) => {
     return callback(direction);
   };
+
+  useEffect(() => {
+    currentMonth < intervalArray[0].orderDate
+      ? setDisablePrev(true)
+      : setDisablePrev(false);
+    currentMonth > intervalArray[intervalArray.length - 1].orderDate
+      ? setDisableNext(true)
+      : setDisableNext(false);
+  }, [intervalArray, currentMonth]);
 
   return (
     <div id="navbar-container">
       <div id="navbar-title">Order Dashboard</div>
       <div id="navbar-main">
         <div id="navbar-date">{toMonthYYYY(currentMonth)}</div>
-        <Arrows callback={handleClick} />
+        <Arrows
+          callback={handleClick}
+          disabledNext={disabledNext}
+          disabledPrev={disabledPrev}
+        />
       </div>
     </div>
   );
