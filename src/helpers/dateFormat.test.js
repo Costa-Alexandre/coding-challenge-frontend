a08c09a0ca3b67dbe91a7ec16fe3a2d0805c5b7d
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-23 15:10:34 +0200

    test: getMonthFromString

--- src/helpers/dateFormat.test.js
+++ src/helpers/dateFormat.test.js
@@ -1,4 +1,4 @@
-import { toDate, toMonthYYYY } from './dateFormat';
+import { toDate, toMonthYYYY, getMonthFromString } from './dateFormat';
 
 describe('toDate()', () => {
   it('should convert string to date', () => {
@@ -13,5 +13,11 @@ describe('toDate()', () => {
     expect(toMonthYYYY(new Date(2021, 1, 25))).toBe('February 2021');
     expect(toMonthYYYY(new Date(2021, 2, 18))).toBe('March 2021');
     expect(toMonthYYYY(new Date(2022, 1, 21))).toBe('February 2022');
+  });
+
+  it('should return correct month Jan=1, Feb=2', () => {
+    expect(getMonthFromString('February')).toBe(2);
+    expect(getMonthFromString('March')).toBe(3);
+    expect(getMonthFromString('December')).toBe(12);
   });
 });
