835a1062a491adc07e176b6aab60e173725f96c8
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-23 21:51:21 +0200

    refactor: removed unused hooks

--- src/components/RightTable.js
+++ src/components/RightTable.js
@@ -1,13 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { currencyFormat } from '../helpers/currencyFormat';
 
 export default function RightTable({ orders, sumOrders }) {
-  useEffect(() => {
-    console.log('top orders', orders);
-  }, [orders]);
-
-  // const progressWidthStyle = { width: `${progress * 100}%` };
-
   const tableRows = orders.map((order, index) => {
     return (
       <tr key={index}>
