cfec44bc4deea4bfea49ec30c2e383f16c9006b4
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-23 20:42:16 +0200

    chore: topOrdersArray return top 5 most sold products in the month

--- src/helpers/filterFunc.js
+++ src/helpers/filterFunc.js
@@ -15,8 +15,38 @@ const filterOrdersMonth = (orders, month, year) => {
   return slicedOrders;
 };
 
+const topOrdersArray = (orders, month, year) => {
+  const monthOrders = orders.filter(
+    (order) =>
+      order.orderDate.getMonth() + 1 === month &&
+      order.orderDate.getFullYear() === year,
+  );
+  const aggregatedOrders = aggregateOrders(monthOrders);
+  const sortedOrders = aggregatedOrders.sort(
+    (b, a) => b.orderVolume - a.orderVolume,
+  );
+  const slicedOrders = sortedOrders.reverse().slice(0, 5);
+  return slicedOrders;
+};
+
 const getOrdersMonth = (orders, month, year) => {
   return sumOrders(filterOrdersMonth(orders, month, year));
 };
 
-export { sumOrders, filterOrdersMonth, getOrdersMonth };
+const aggregateOrders = (orders) => {
+  const orderProducts = orders.map((order) => order.product);
+  const uniqueProducts = orderProducts.filter(
+    (product, index, self) => self.indexOf(product) === index,
+  );
+  const uniqueProductsOrders = uniqueProducts.map((product) => {
+    return {
+      product,
+      orderVolume: orders
+        .filter((order) => order.product === product)
+        .reduce((acc, order) => acc + order.orderVolume, 0),
+    };
+  });
+  return uniqueProductsOrders;
+};
+
+export { sumOrders, filterOrdersMonth, getOrdersMonth, topOrdersArray };
