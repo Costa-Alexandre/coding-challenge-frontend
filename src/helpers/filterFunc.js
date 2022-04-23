6f0a81f2f6b3d34f8180362334fdae9f58de2bf4
Alexandre Costa <alexandre.costa@code.berlin>
2022-04-23 14:56:03 +0200

    fix: remove console.log

--- src/helpers/filterFunc.js
+++ src/helpers/filterFunc.js
@@ -1,7 +1,5 @@
 const sumOrders = (orders) => {
   const result = orders.reduce((acc, order) => {
-    console.log('acc', acc);
-    console.log('order Volume', order.orderVolume);
     return acc + order.orderVolume;
   }, 0);
   return result;
